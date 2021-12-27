668c4d965f035eae69fdd91a1aea5555973740da
jino <jho378@naver.com>
2021-12-28 08:51:42 +0900

    [chore] module utils로 분리, file에서 데이터 관리

--- initialize.js
+++ initialize.js
@@ -1,8 +1,12 @@
-const {Asset, Book, Coin, Key, Stock, User, Schedule} = require('./models');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
+
+const {Asset, Book, Coin, Key, Stock, User, Schedule} = require('./models');
+const {getMainHTML, parsing, getSubHTML, subParsing} = require('./utils');
 let start = 0;
 let end = 0;
+
 const init = async() => {
     start = new Date();
     await Asset.deleteMany();
@@ -11,78 +15,42 @@ const init = async() => {
     await Key.deleteMany();
     await Stock.deleteMany();
     await User.deleteMany();
-    await Schedule.deleteMany();
-
-    
+    await Schedule.deleteMany();    
+    fs.writeFileSync('./datas/stocks.json', JSON.stringify([]));
     const coins = ['bitcoin', 'ethereum', 'dogecoin', 'ripple', 'solana'];
     const stocks = ['COST', 'MSFT', 'GOOG', 'AMZN', 'TSLA', 'NKE', 'IONQ', 'PLTR', 'SBUX', 'AAPL', 'KO', 'JPM','BRK-B', 'AXP', 'O', 'PG', 'ABNB'];
     // maybe add ETF later;
     // https://companiesmarketcap.com/assets-by-market-cap/
-    const stockNames = [];
-    const stockSectors = [];
-    const stockIndustries = [];
+    
     for(const _coin of coins){
-        const coin = new Coin({name : _coin, sector: 'Crypto', industry : 'Crypto', isActive : true});
+        const coin = new Coin({name : _coin, sector: 'Crypto', industry : 'Crypto', assetType : 'cryptocoin', isActive : true});
         await coin.save();
     }
     console.log('coins initialization completed');
 
-    const getMainHTML = async(ticker) => {
-        try{
-            return await axios.get(`https://finance.yahoo.com/quote/${ticker}`);
-        }   catch(err){
-            console.log({error : 'Error occured when parsing main html'});
-        }
-    }
-    
-    const parsing = async(ticker) => {
-        const html = await getMainHTML(ticker);
-        const $ = cheerio.load(html.data);
-        const $price = $("#quote-header-info");
-        $price.each((idx, node) => {
-            const priceText = $(node).text();
-            const stockName = priceText.slice(0, priceText.indexOf('(')-1);
-            console.log(stockName);
-            stockNames.push(stockName);
-        })
-    }
-
-    const getSubHTML = async(ticker) => {
-        try{
-            return await axios.get(`https://finance.yahoo.com/quote/${ticker}/profile?p=${ticker}`);
-        }   catch(err){
-            console.log({error : 'Error occured when parsing sub html'});
-        }
-    }
-
-    const subParsing = async(ticker) => {
-        const html = await getSubHTML(ticker);
-        const $ = cheerio.load(html.data);
-        const $field = $(".asset-profile-container");
-        $field.each((idx, node) => {
-            const fieldText = $(node).text();
-            const fieldArr = fieldText.split('Industry');
-            const sector = fieldArr[0].slice(fieldArr[0].indexOf("):") + 3);
-            const industry = fieldArr[1].slice(2, fieldArr[1].indexOf('Full Time'))
-            stockSectors.push(sector);
-            stockIndustries.push(industry);
-        })
-    }
-
     for(const _stock of stocks){
-        try{
+        // try{
         await parsing(_stock);
         await subParsing(_stock);
-        const stock = new Stock({name : stockNames[stockNames.length -1],
-            ticker : _stock,
-            sector : stockSectors[stockSectors.length-1],
-            industry : stockIndustries[stockIndustries.length -1],
-            isActive : true});
-        await stock.save();
-        }   catch(err){
-            console.log({error: 'Might have typed wrong ticker'});
-        }
+        // }   catch(err){
+        //     console.log({error: 'Might have typed wrong ticker'});
+        // }
+        // later, test if getting values from datas/json works
     }
+
+    let stocksJson = fs.readFileSync('./datas/stocks.json').toString();
+    stocksJson = JSON.parse(stocksJson);
+    stocksJson.forEach(async e => {
+        const stock = new Stock({
+            name : e.name,
+            ticker : e.ticker,
+            sector : e.sector,
+            industry : e.industry,
+            assetType : "stock",
+            isActive : true,    
+        });
+        await stock.save();
+    })
     
     console.log('stocks initialization completed');   
     end = new Date(); 
@@ -91,5 +59,4 @@ const init = async() => {
 
 init();
 
-// done by now 
-// 나중에 update하고 싶을떄 여기서 돌리면 재앙이야 나중에 update 할때 따로 만들어서 하세요 
+// done by now 21.12.27
--- utils.js
+++ utils.js
@@ -2,6 +2,10 @@ const crypto = require('crypto');
 const User = require('./models/User');
 const Key = require('./models/Key');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
 const encryptPassword = (password) => {
     return crypto.createHash('sha512').update(password).digest('base64');
 }
@@ -30,7 +34,60 @@ const setAuth = async(req, res, next) => {
 }
 
 
+const getMainHTML = async(ticker) => {
+    try{
+        return await axios.get(`https://finance.yahoo.com/quote/${ticker}`);
+    }   catch(err){
+        console.log({error : 'Error occured when parsing main html'});
+    }
+}
+
+const parsing = async(ticker) => {
+    const html = await getMainHTML(ticker);
+    const $ = cheerio.load(html.data);
+    const $price = $("#quote-header-info");
+    $price.each((idx, node) => {
+        const priceText = $(node).text();
+        const stockName = priceText.slice(0, priceText.indexOf('(')-1);
+        console.log(stockName);
+        let stocksJson = fs.readFileSync('./datas/stocks.json').toString();
+        stocksJson = JSON.parse(stocksJson);
+        stocksJson.push({ticker : ticker, name : stockName});
+        fs.writeFileSync('./datas/stocks.json', JSON.stringify(stocksJson));
+    })
+}
+
+const getSubHTML = async(ticker) => {
+    try{
+        return await axios.get(`https://finance.yahoo.com/quote/${ticker}/profile?p=${ticker}`);
+    }   catch(err){
+        console.log({error : 'Error occured when parsing sub html'});
+    }
+}
+
+const subParsing = async(ticker) => {
+    const html = await getSubHTML(ticker);
+    const $ = cheerio.load(html.data);
+    const $field = $(".asset-profile-container");
+    $field.each((idx, node) => {
+        const fieldText = $(node).text();
+        const fieldArr = fieldText.split('Industry');
+        const sector = fieldArr[0].slice(fieldArr[0].indexOf("):") + 3);
+        const industry = fieldArr[1].slice(2, fieldArr[1].indexOf('Full Time'))
+        let stocksJson = fs.readFileSync('./datas/stocks.json').toString();
+        stocksJson = JSON.parse(stocksJson);
+        const index = stocksJson.findIndex(e => e.ticker === ticker);
+        stocksJson[index].sector = sector;
+        stocksJson[index].industry = industry;
+        fs.writeFileSync('./datas/stocks.json', JSON.stringify(stocksJson));
+    })
+}
+
 module.exports = {
     encryptPassword,
     setAuth, 
+    getMainHTML,
+    parsing,
+    getSubHTML,
+    subParsing,
 }
