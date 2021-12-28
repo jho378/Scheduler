a85121f2593707070a9401279b54935d5989f7ae
jino <jho378@naver.com>
2021-12-28 13:53:19 +0900

    [feature] signup.js 작성

---
+++ routes/signup.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const {body, validationResult} = require('express-validator');
+
+const {Asset, Coin, Stock, User} = require('../models');
+const { encryptPassword } = require('../utils');
+
+router.post('/', 
+body('id')
+    .isAlphanumeric()
+    .withMessage('error : user ID must be alphanumeric')
+    .isLength({min : 8, max : 20})
+    .withMessage('error : user ID should be longer than 8 words, shorter than 20 words'),
+body('password')
+    .isLength({min : 8, max : 16})
+    .withMessage('error : Password should be longer than 8 words, shorter than 16 words.'),
+body('username')
+    .isAlphanumeric()
+    .withMessage('error : Name must be alphanumeric')
+    .isLength({min : 1, max : 10})
+    .withMessage('error : Username must be longer than 1 word, shorter than 10 words'),
+async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send({errors : errors.array()});
+    }
+    const {id, password, username} = req.body;
+    const encryptedPassword = encryptPassword(password);
+    let user = null;
+    try{
+        user = new User({
+        id,
+        password,
+        username,
+        isDeleted : false,
+    });
+    await user.save();
+    }   catch(err){
+        return res.status(400).send({error : 'Email is duplicated.'})
+    }
+    const stocks = await Stock.find({isActive : true});
+    for(const stock of stocks){
+        const asset = new Asset({
+            name : stock.name, 
+            ticker : stock.ticker, 
+            sector : stock.sector, 
+            industry : stock.industry,
+            averagePrice : 0,
+            balance : 0,
+            assetType : "stock",
+            user,
+        });
+        await asset.save();
+    }
+    const coins = await Coin.find({isActive : true});
+    for(const coin of coins){
+        const asset = new Asset({
+            name : coin.name,
+            sector : coin.sector,
+            industry : coin.industry,
+            averagePrice : 0,
+            balance : 0,
+            assetType : "cryptocoin",
+            user,
+        })
+        await asset.save();
+    }
+    return res.send(200);
+})
+
+module.exports = router;
