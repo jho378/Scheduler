e40738210df1f8ad8c116e5a2cd0a6cfe3b8100e
김진호 <jinho@gimjinhoui-MacBookPro.local>
2021-12-29 14:30:42 +0900

    [feature] dotenv 설정 및 코인초기화 추가

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const Asset = require('./Asset');
 const Book = require('./Book');
 const Coin = require('./Coin');
@@ -8,7 +7,9 @@ const Stock = require('./Stock');
 const User = require('./User');
 const Schedule = require('./Schedule');
 
-const mongoURL = 'mongodb://admin:<pw>@calendar-shard-00-00.pfpok.mongodb.net:27017,calendar-shard-00-01.pfpok.mongodb.net:27017,calendar-shard-00-02.pfpok.mongodb.net:27017/calendarServer?ssl=true&replicaSet=atlas-nlsbae-shard-0&authSource=admin&retryWrites=true&w=majority';
+require('dotenv').config();
+
+const mongoURL = `mongodb://admin:${process.env.DB_PASS}@calendar-shard-00-00.pfpok.mongodb.net:27017,calendar-shard-00-01.pfpok.mongodb.net:27017,calendar-shard-00-02.pfpok.mongodb.net:27017/calendarServer?ssl=true&replicaSet=atlas-nlsbae-shard-0&authSource=admin&retryWrites=true&w=majority`;
 mongoose.connect(mongoURL);
 if(mongoose.connect(mongoURL))  console.log('connected to DB');
 
