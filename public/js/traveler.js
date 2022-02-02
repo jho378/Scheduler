6d4f440aaa6d92922a620bae85541a877a667c12
김진호 <jho378@naver.com>
2022-02-02 13:06:48 +0900

    [chore] map data to html

--- public/js/traveler.js
+++ public/js/traveler.js
@@ -1,33 +1,25 @@
-const fs =require('fs');
-const axios = require('axios');
-const API_KEY = `fb76836f62cc4e7781941e901500dc9b`;
-const MAX_LAT = 90;
-const MAX_LON = 180;
-const scale = 2;
+const fs = require('fs');
 
-const Map = async () => {
-    let map = fs.readFileSync('../../datas/map.json');
-    map = JSON.parse(map);
+// const map = document.querySelector('.map');
+let countries = ["Sea","Canada","Greenland","Russia","Norway","United States","Finland","Sweden","Iceland","United Kingdom","Estonia","Denmark","Lithuania","Latvia","Belarus","Ireland","Germany","Poland","Kazakhstan","Netherlands","Ukraine","China","France","Luxembourg","Czechia","Mongolia","Austria","Slovakia","Hungary","Moldova","Italy","Slovenia","Croatia","Serbia","Romania","Bosnia and Herzegovina","Uzbekistan","Japan","Spain","Albania","North Macedonia","Bulgaria","Turkey","Georgia","Turkmenistan","Kyrgyzstan","North Korea","Portugal","Greece","Azerbaijan","Iran","Tajikistan","South Korea","Algeria","Tunisia","Malta","Syria","Iraq","Afghanistan","Pakistan","Morocco","Lebanon","India","Mexico","Libya","Jordan","Saudi Arabia","Egypt","Nepal","Bhutan","Myanmar","Sahrawi Arab Democratic Republic","Mauritania","United Arab Emirates","Taiwan","The Bahamas","Mali","Oman","Bangladesh","Cuba","Niger","Chad","Sudan","Laos","Vietnam","Dominican Republic","Thailand","Luzon","Belize","Jamaica","Haiti","Yemen","Philippines","Guatemala","Honduras","Cape Verde","Senegal","Eritrea","Nicaragua","Burkina Faso","Ethiopia","Cambodia","Colombia","Venezuela","Grenada","Guinea-Bissau","Guinea","Nigeria","Costa Rica","Côte d'Ivoire","Ghana","Benin","Cameroon","Central African Republic","South Sudan","Somalia","Sri Lanka","Visayas","Federated States of Micronesia","Marshall Islands","Panama","Guyana","Sierra Leone","Liberia","Suriname","Malaysia","Brazil","Democratic Republic of the Congo","Democratic Republic of Congo","Uganda","Kenya","Indonesia","Palau","Equatorial Guinea","Gabon","Congo-Brazzaville","Ecuador","Congo","Peru","Rwanda","Tanzania","Papua New Guinea","Burundi","Angola","Solomon Islands","Kiribati","Bolivia","Zambia","Malawi","Australia","Mozambique","Samoa","Madagascar","Vanuatu","Tonga","Zimbabwe","Namibia","Fiji","Cook Islands","Chile","Paraguay","Botswana","Argentina","South Africa","Eswatini","Lesotho","Uruguay","New Zealand","Falkland Islands","Antarctica"]
 
-    const lon = map[map.length-1].length * scale - MAX_LON;
-    const lat = MAX_LAT - (map.length * scale);
-
-    if(lon === MAX_LON){
-        if(lat > -MAX_LAT){
-            map.push([]);
-            fs.writeFileSync('../../datas/map.json', JSON.stringify(map));
-            Map();
-            return ; 
-        }
+let mapData = fs.readFileSync('../../datas/map.json');
+mapData = JSON.parse(mapData);
+for(let i=0; i<90; i++){
+    for(let j=0; j<180; j++){
+        if(!countries.includes(`"${mapData[i][j]}"`)) countries.push(`"${mapData[i][j]}"`)
+        if(mapData[i][j]==='Sea')   mapData[i][j] = `<div class='country' id='Sea'></div>`
+        else mapData[i][j] = `<div class='country' id=${mapData[i][j]}></div>`;
     }
-    const url = `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=${API_KEY}`;
-
-    const mapData = await axios.get(url).then(res => res.data).then(result => result.results[0].country).then(country => {
-        console.log(lat, lon, country);
-        if(country) map[map.length-1].push(country);
-        else map[map.length-1].push('Sea');
-    });
-    fs.writeFileSync('../../datas/map.json', JSON.stringify(map));
-    Map();
 }
-Map();
+
+for(let i=3; i<90; i++){
+    mapData[i] = mapData[i].join('\n');
+}
+const _arr = mapData.slice(3, 77);
+const __arr = mapData.slice(77);
+const res = __arr.concat(_arr);
+const maps = res.join('\n');
+
+fs.writeFileSync('../../datas/maps.json',String(countries));
+// fs.writeFileSync('../../datas/maps.json', maps)
