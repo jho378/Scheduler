c5e0b6ca539195e07fee85881b6a99ec178367c4
김진호 <jho378@naver.com>
2022-02-04 22:55:30 +0900

    [chore] temporal try

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,44 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+// router.get('/', (req, res) => {
+//     res.send('This is index page!');
+// })
 
-router.get('/', (req, res) => res.send('This is index page!'));
+router.get('/', (req, res) => {
+    fs.readFile(path.join(__dirname, "../public/html/index.html"), (err, data) => {
+        if(err){
+            console.log(err);
+            console.log('error occured at path /:month');
+            res.status(400).send('Invalid Month');
+        }   else{
+            res.end(data);
+        }
+    })
+})
+router.get('/traveler', (req, res) => {
+    fs.readFile(path.join(__dirname, "../public/html/traveler.html"), (err, data) => {
+        if(err){
+            console.log(err);
+            console.log('error occured at path /traveler');
+            res.status(400).send('Invalid uri');
+        }   else{
+            res.end(data);
+        }
+    })
+})
+
+router.get('/:month', async(req, res) => {
+    const {month} = req.params;
+    fs.readFile(__dirname + "../public/html/index.html", (err, data) => {
+        if(err){
+            console.log('error occured at path /:month');
+            res.status(400).send('Invalid Month');
+        }   else{
+            res.end(data);
+        }
+    })
+})
 
 module.exports = router;
