86fbcb5df5ed279d0224f40439e9037c7a2d7444
김진호 <jho378@naver.com>
2022-02-19 12:06:49 +0900

    [chore] minor fix

--- routes/signup.js
+++ routes/signup.js
@@ -12,21 +12,21 @@ router.post(
         .withMessage('error : user ID must be alphanumeric')
         .isLength({ min: 4, max: 20 })
         .withMessage(
-            'error : user ID should be longer than 8 words, shorter than 20 words'
+            'error : user ID should be longer than 4 words, shorter than 20 words'
         ),
     body('password')
         .isLength({ min: 6, max: 16 })
         .withMessage(
-            'error : Password should be longer than 8 words, shorter than 16 words.'
+            'error : Password should be longer than 6 words, shorter than 16 words.'
         ),
-    body('last-name')
+    body('lastname')
         .isAlphanumeric()
         .withMessage('error : Name must be alphanumeric')
         .isLength({ min: 1, max: 10 })
         .withMessage(
             'error : last name must be longer than 1 word, shorter than 10 words'
         ),
-    body('first-name')
+    body('firstname')
     .isAlphanumeric()
     .withMessage('error : Name must be alphanumeric')
     .isLength({ min: 1, max: 10 })
@@ -38,14 +38,27 @@ router.post(
         if (!errors.isEmpty()) {
             return res.status(400).send({ errors: errors.array() });
         }
-        const { id, password, username } = req.body;
+        const { id, password, passwordConfirm, lastname, firstname, gender } = req.body;
+        const yy = Number(req.body.yy);
+        const mm = Number(req.body.mm) -1;
+        const dd = Number(req.body.dd) +1;
+        console.log(yy, mm, dd)
+        if(password!==passwordConfirm){
+            return res.status(400).send({error : 'Please check if you have written same password in the confirmation.'});
+        }
         const encryptedPassword = encryptPassword(password);
+        
+        const birthday = new Date(yy, mm, dd);
         let user = null;
         try {
             user = new User({
                 id,
                 password: encryptedPassword,
-                username,
+                lastname,
+                firstname,
+                birthday,
+                created : new Date(), 
+                gender,
                 isDeleted: false,
             });
             await user.save();
@@ -79,7 +92,9 @@ router.post(
             });
             await asset.save();
         }
-        return res.send(user);
+        
+        // return res.send(user);
+        return res.redirect('/');
     }
 );
 
