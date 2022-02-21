e8d6b805c6bd1e56d3485ba38897ae905f72bd58
김진호 <jho378@naver.com>
2022-02-21 20:28:07 +0900

    [chore] minor

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,21 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const { setAuth } = require('../utils');
+const cookieParser = require('cookie-parser')
 // router.get('/', (req, res) => {
 //     res.send('This is index page!');
 // })
 
-router.get('/', (req, res) => {
+router.get('/', setAuth, (req, res) => {
     fs.readFile(path.join(__dirname, "../public/html/index.html"), (err, data) => {
         if(err){
             console.log(err);
             console.log('error occured at path /:month');
             res.status(400).send('Invalid Month');
         }   else{
+            // console.log(req.headers.authorization);
+            console.log(req.cookies)
             res.end(data);
         }
     })
