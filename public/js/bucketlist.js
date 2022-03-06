4556bdcda15feecf9cc7586ddc5512e153425be4
김진호 <jho378@naver.com>
2022-03-06 14:31:42 +0900

    [feature] bucketlist view

--- public/js/bucketlist.js
+++ public/js/bucketlist.js
@@ -20,3 +20,42 @@ const createElem = () => {
 // createBtn.addEventListener('click', () => create());
 
 // createBtn.addEventListener('click', () => createElem());
+const formatBtn = document.querySelectorAll('.bucketlist_view button');
+const changeFormat = (event) => {
+    const grid = document.querySelector('.bucketlist_view button.grid');
+    const text = document.querySelector('.bucketlist_view button.text');
+    const theme = document.querySelector('.bucketlist_view button.theme');
+    const changeActive = event.target;
+    if(changeActive===grid){
+        changeActive.classList.add('active')
+        text.classList.remove('active');
+        theme.classList.remove('active');
+    }
+    else if(changeActive===text){
+        changeActive.classList.add('active')
+        grid.classList.remove('active');
+        theme.classList.remove('active');
+    }
+    else if(changeActive===theme){
+        changeActive.classList.add('active')
+        text.classList.remove('active');
+        grid.classList.remove('active');
+    }
+    // changeActive.classList.add('active');
+    // changeActive.classList.add('')
+    // console.log(event.target.classList);
+}
+formatBtn.forEach(btn => btn.addEventListener('click', changeFormat));
+
+
+const wishlists = document.querySelector('.wishlists');
+const wishlist = new Array(100);
+for(let i=0; i<100; i++){
+    wishlist.push(
+    `<div class="wishlist">
+    <h3 class="detail">Click to Add bucketlist</h3>
+    <div class="img"></div>
+    </div>`
+    )
+}
+wishlists.innerHTML += wishlist.join('');
